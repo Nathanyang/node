d036f634cc26101684662260a5d5b685378bd23b
nathan <nathanyang@lunchactually.com>
2015-02-26 17:19:16 +0800

    update poster

--- app.js
+++ app.js
@@ -21,32 +21,32 @@ app.get("/", function(req, res){
 		movies: [{
 			title: '分歧者:异类觉醒',
 			_id: 1,
-			poster: 'http://www.youku.com/show_page/id_z7d8bd9f20ee111e3a705.html'
+			poster: 'http://r2.ykimg.com/051600005402ED6267379F1062086E08'
 		},
 		{
 			title: '分歧者:异类觉醒',
 			_id: 2,
-			poster: 'http://www.youku.com/show_page/id_z7d8bd9f20ee111e3a705.html'
+			poster: 'http://r2.ykimg.com/051600005402ED6267379F1062086E08'
 		},
 		{
 			title: '分歧者:异类觉醒',
 			_id: 3,
-			poster: 'http://www.youku.com/show_page/id_z7d8bd9f20ee111e3a705.html'
+			poster: 'http://r2.ykimg.com/051600005402ED6267379F1062086E08'
 		},
 		{
 			title: '分歧者:异类觉醒',
 			_id: 4,
-			poster: 'http://www.youku.com/show_page/id_z7d8bd9f20ee111e3a705.html'
+			poster: 'http://r2.ykimg.com/051600005402ED6267379F1062086E08'
 		},
 		{
 			title: '分歧者:异类觉醒',
 			_id: 5,
-			poster: 'http://www.youku.com/show_page/id_z7d8bd9f20ee111e3a705.html'
+			poster: 'http://r2.ykimg.com/051600005402ED6267379F1062086E08'
 		},
 		{
 			title: '分歧者:异类觉醒',
 			_id: 6,
-			poster: 'http://www.youku.com/show_page/id_z7d8bd9f20ee111e3a705.html'
+			poster: 'http://r2.ykimg.com/051600005402ED6267379F1062086E08'
 		}]
 	})
 })
@@ -59,7 +59,7 @@ app.get("/movie/:id", function(req, res){
 			country: '美国',
 			title: '分歧者:异类觉醒',
 			year: '2014',
-			poster: 'http://www.youku.com/show_page/id_z7d8bd9f20ee111e3a705.html',
+			poster: 'http://r2.ykimg.com/051600005402ED6267379F1062086E08',
 			language: '英语',
 			flash: 'http://player.youku.com/player.php/sid/XODU1MDE0MjM2/v.swf',
 			summary: '一百年前，席卷全球的战争让几乎所有的国家都从地球上消失，幸存的人类聚集在破败的芝加哥。新世界的创始者为了谋求永久的和平，将幸存者们分成了无私派、诚实派、无畏派、友好派和博学派。每个派系都有自己专门的社会职责，他们通力合作促进这个世界的进步。美丽女孩碧翠丝（谢琳·伍德蕾 饰）出生于无私派家庭，她和其他同龄人一样，将在16岁那年决定自己终身所属的派系。然而测试表明她是一个极为少见的...'
