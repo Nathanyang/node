478c542133da6b53fc439d5dab354e77c4b18335
nathan <nathanyang@lunchactually.com>
2015-02-27 19:45:48 +0800

    adminUser

--- public/js/adminUser.js
+++ public/js/adminUser.js
@@ -2,11 +2,11 @@ $(function(){
     $('.del').click(function(e){
         var target = $(e.target);
         var name = target.data('id');
-		var tr = $('.item-id-' + id);
+		var tr = $('.item-id-' + name);
 	
 		$.ajax({
 	        type: 'DELETE',
-		    url: '/admin/user/del?name=' + id
+		    url: '/admin/user/del?name=' + name
 		})
 		.done(function(results){
 		    if(results.success === 1){
