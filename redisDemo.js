2fa9b8489729701626ba4b208a2adb6c1489fbdc
nathan <nathanyang@lunchactually.com>
2015-03-02 14:43:47 +0800

    up redis Demo

--- redisDemo.js
+++ redisDemo.js
@@ -5,7 +5,8 @@
  */
 var http = require("http");
 var redis = require("redis");
-var client = redis.createClient();
+//var client = redis.createClient();
+var client = redis.createClient(6379, "121.40.83.122:3000");
 var i = 1;
 
 http.createServer(
@@ -14,12 +15,24 @@ http.createServer(
         response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
 
         response.write("欢迎您第" + i + "次访问服务器！<br>Welcome!<br>");
-        client.set(["demo2_key", "服务器访问被访问次数: i = " + i]);
-        client.get(["demo2_key"], function (err, result) {
-            console.log(result);
-            response.write(result);
-            response.end();
+
+        client.on("error", function(err){
+            console.log("Error: " + err);
         });
+
+        client.on("connect", function(){
+            // start server();
+            client.set("demo2_key", "服务器访问被访问次数: i = " + i, function(){
+                console.log(reply.toString());
+            });
+
+            client.get("demo2_key", function (err, result) {
+                console.log(result);
+                response.write(result);
+                response.end();
+            });
+        });
+
         i++;
     }).listen(3000);
 
